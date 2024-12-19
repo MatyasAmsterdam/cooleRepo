5f87ea31679b87efe2e4d37e293fc2126e819eed
MatyasAmsterdam <70918594+MatyasAmsterdam@users.noreply.github.com>
2024-12-19 15:15:23 +0100

    Update assert_even_time.py

--- assert_even_time.py
+++ assert_even_time.py
@@ -1,3 +1,5 @@
 import datetime
 
-assert int(datetime.datetime.now().strftime('%M')) % 2 == 0
+
+assert int(datetime.datetime.now().strftime('%M')) % 2 == 0, \
+"Current datetime is an uneven number which we have deemed improper for some reason."
